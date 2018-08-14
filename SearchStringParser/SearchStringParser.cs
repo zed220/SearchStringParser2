76747f92322fc811ac3b615d6777d1178fc7697f
Piter Z <pzed220@gmail.com>
2018-08-14 17:48:09 +0300

    start

---
+++ SearchStringParser/SearchStringParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchStringParser {
+    public class SearchStringParser {
+        SearchStringParser() { }
+
+        public static SearchStringParseResult Parse(string text, SearchStringParseSettings settings) {
+            return new SearchStringParser().ParseCore(text, settings);
+        }
+
+        SearchStringParseResult ParseCore(string text, SearchStringParseSettings settings) {
+            return null;
+        }
+    }
+
+    public class SearchStringParseResult {
+        public List<SearchStringFieldParseInfo> ForAll { get; } = new List<SearchStringFieldParseInfo>();
+        public Dictionary<string, List<SearchStringFieldParseInfo>> FieldSpecific { get; } = new Dictionary<string, List<SearchStringFieldParseInfo>>();
+    }
+
+    public class SearchStringFieldParseInfo {
+        public SearchStringFieldParseInfo(SearchMode searchMode) {
+            SearchMode = searchMode;
+        }
+
+        public List<string> SearchStrings { get; } = new List<string>();
+        public SearchMode SearchMode { get; }
+    }
+
+    public enum SearchMode {
+        Like,
+        Contains,
+        Equals,
+        StartsWith,
+        EndsWith
+    }
+
+    public class SearchStringParseSettings {
+        public static SearchStringParseSettings Default {
+            get {
+                return new SearchStringParseSettings();
+            }
+        }
+    }
+}
---
+++ SearchStringParser/Tests/ParseTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchStringParser.Tests {
+    public class ParseTests {
+        static SearchStringParseResult Parse(string text, SearchStringParseSettings settings) {
+            return SearchStringParser.Parse(text, settings);
+        }
+
+        [Test]
+        public void SingleChar() {
+            var result = Parse("a", SearchStringParseSettings.Default);
+            Assert.AreEqual(1, result.ForAll.Count);
+            Assert.AreEqual(1, result.ForAll.Single().SearchStrings.Count);
+            Assert.AreEqual("a", result.ForAll.Single().SearchStrings.Single());
+            Assert.AreEqual(SearchMode.Like, result.ForAll.Single().SearchMode);
+        }
+    }
+}
